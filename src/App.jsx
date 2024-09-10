f123eff421f5023a098063bee9c14d3213b32105
Addy Rodriguez <rdz.33.a@gmail.com>
2024-09-10 11:32:26 -0500

    fixed issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Article1 from './components/Article1'
 import Article2 from './components/Article2'
 import Footer from './components/Footer'
-import Header from './components/header'
+import Header from './components/Header'
 
 
 
